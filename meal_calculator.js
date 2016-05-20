3726533932b892f775eb441b2442aa2a9a2aba81
jon-wade <jonwadeuk@gmail.com>
2016-05-20 11:36:48 +0100

    complete calculator

--- meal_calculator.js
+++ meal_calculator.js
@@ -12,3 +12,71 @@
  Print a breakdown for what each diner owes
  */
 
+
+var Diner = function(name){
+    this.name = name;
+    this.mealRecord = {};
+    this.mealRecord.dishName = [];
+    this.mealRecord.dishPrice = [];
+    this.addDish = function(dishName, dishPrice){
+        this.mealRecord.dishName.push(dishName);
+        this.mealRecord.dishPrice.push(dishPrice);
+    };
+    this.mealTotal = function(){
+        var total = 0;
+        this.mealRecord.dishPrice.forEach(function(item){
+            total += item;
+        });
+
+        //add 20% tax for each diner
+        total *= 1.2;
+
+        return total;
+    };
+};
+
+
+var Meal = function() {
+
+    var args = Array.prototype.slice.call(arguments);
+
+    this.mealTotal = function(){
+        var mealTotal = 0;
+        for (var i=0; i<args.length; i++){
+            //console.log(args[i]);
+            mealTotal += args[i].mealTotal();
+        }
+        return mealTotal;
+    };
+
+    this.tip = function(tipPercent){
+        return this.mealTotal() * (tipPercent/100);
+    };
+
+    this.totalBill = function(tipPercent){
+        return this.mealTotal() + this.tip(tipPercent);
+    };
+};
+
+var diner1 = new Diner('Jon');
+
+diner1.addDish('curry', 20);
+diner1.addDish('fish & chips', 40);
+
+var diner2 = new Diner('Cath');
+
+diner2.addDish('pasta', 30);
+diner2.addDish('steak', 80);
+
+var meal = new Meal(diner1, diner2);
+
+console.log(diner1.name, diner1.mealRecord.dishName, diner1.mealRecord.dishPrice, diner1.mealTotal());
+console.log(diner2.name, diner2.mealRecord.dishName, diner2.mealRecord.dishPrice, diner2.mealTotal());
+console.log('TOTAL BILL = ', meal.totalBill(20));
+console.log('DINER 1 FAIR SHARE OF BILL PLUS 50% OF TIP', diner1.mealTotal() + (meal.tip(20)/2));
+console.log('DINER 2 FAIR SHARE OF BILL PLUS 50% OF TIP', diner2.mealTotal() + (meal.tip(20)/2));
+
+
+
+
+
